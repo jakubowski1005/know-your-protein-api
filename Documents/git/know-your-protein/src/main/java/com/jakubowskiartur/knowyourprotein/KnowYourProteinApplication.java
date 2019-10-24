f860e6d443ee6b9bedcc847f3fefa24bc2d0e75b
jakubowski1005 <jakubowski1005@gmail.com>
2019-10-24 13:46:43 +0200

    Add gradle project structure files

---
+++ Documents/git/know-your-protein/src/main/java/com/jakubowskiartur/knowyourprotein/KnowYourProteinApplication.java
@@ -0,0 +1,13 @@
+package com.jakubowskiartur.knowyourprotein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowYourProteinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowYourProteinApplication.class, args);
+	}
+
+}
---
+++ Documents/git/know-your-protein/src/test/java/com/jakubowskiartur/knowyourprotein/KnowYourProteinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubowskiartur.knowyourprotein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowYourProteinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
