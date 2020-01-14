a3a81db8df3ebf85f52f7ca445c0cfcde37b040c
jakubowski1005 <jakubowski1005@gmail.com>
2020-01-14 20:19:12 +0100

    Heroku commit

--- src/main/java/com/jakubowskiartur/knowyourprotein/config/WebSecurityConfig.java
+++ src/main/java/com/jakubowskiartur/knowyourprotein/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        
+
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers("/**").permitAll();
 
@@ -25,7 +25,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    // To enable CORS
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
